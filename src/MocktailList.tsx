aa77d348a7c26f8fe62695af8d175b64a0d3b5e5
Simon <sillen.00@hotmail.com>
2023-11-21 12:29:20 +0100

    Refactor MocktailList component and styles

--- src/MocktailList.tsx
+++ src/MocktailList.tsx
@@ -1,36 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./MocktailList.css";
+
+interface Drinks {
+    idDrink: string;
+    strDrink: string;
+    strDrinkThumb: string;
+}
 
 function MocktailList() {
     const [searchbarValue, setsearchbarValue] = useState("");
+    const [mocktails, setMocktails] = useState<Drinks[]>([]);
+
+    useEffect(() => {
+        // Fetch data from the API
+        fetch(
+            "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+        )
+            .then((response) => response.json())
+            .then((data) => setMocktails(data.drinks))
+            .catch((error) => console.error("Error fetching data:", error));
+    }, []);
 
     return (
         <div>
-            <input
-                id="APISearchbar"
-                type="text"
-                name="Searchbar"
-                value={searchbarValue}
-                onChange={(e) => setsearchbarValue(e.target.value)}
-            />
+            <div className="APISearchbarWrapper">
+                <input
+                    id="APISearchbar"
+                    type="text"
+                    name="Searchbar"
+                    value={searchbarValue}
+                    onChange={(e) => setsearchbarValue(e.target.value)}
+                />
+            </div>
 
             <main>
-                <div className="wrapper">
-                    <div className="mocktails">
-                        <h2>Mark's To-drink list</h2>
-                        <ul>
-                            <li>Mocktail 1</li>
-                            <li>Mocktail 2</li>
-                            <li>Mocktail 3</li>
-                            <li>Mocktail 4</li>
-                            <li>Mocktail 5</li>
-                            <li>Mocktail 6</li>
-                            <li>Mocktail 7</li>
-                            <li>Mocktail 8</li>
-                            <li>Mocktail 9</li>
-                            <li>Mocktail 10</li>
-                        </ul>
-                    </div>
+                <div className="mocktailsWrapper">
+                    <h2>Mark's To-drink list</h2>
+                    <ul>
+                        {mocktails.map((mocktail) => (
+                            <li key={mocktail.idDrink} className="mocktailTodo">
+                                <div>
+                                    <img
+                                        src={mocktail.strDrinkThumb}
+                                        alt={mocktail.strDrink}
+                                    />
+                                    <h3>{mocktail.strDrink}</h3>
+                                </div>
+                                <div className="buttonDiv">
+                                    <button className="mocktailCompleted">
+                                        Complete
+                                    </button>
+                                    <button className="mocktailDelete">
+                                        Delete
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </main>
         </div>
